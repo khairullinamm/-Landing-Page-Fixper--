f5fd81c7686c8652e88b1873454d428713f103a2
Milana Khairullina <hmm2002@yandex.ru>
2023-07-05 02:06:49 +0300

    add: add main part of adaprive  for 780px

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ selectItem.forEach(item => { item.addEventListener("click", () => {
 });
 });
 
+//при нажатии на любое другое место будет скрываться
 document.addEventListener("click", function (e) { 
 
     if (e.target.className !== 'select__body' && e.target.className !== 'select__item' && e.target.className !== 'select__header' && e.target.className !== 'select__current')
@@ -31,3 +32,83 @@ document.addEventListener("click", function (e) {
 
 //Решить проблему в select
 //Изменить цвет крестика при нажатии в discount
+
+
+/*------------------------------ALERT-------------------------------*/
+const ButtonCall = document.querySelectorAll('.button-call');
+const ButtonDiscount = document.querySelectorAll('.button__discount');
+const Alert = document.querySelectorAll('.alert');
+
+
+Alert.forEach(item => item.classList.add("select-hide"));
+
+/*При нажатии на кнопку звонка или скидки вылезает окно*/
+ButtonCall.forEach(item => { item.addEventListener("click", () => {
+    Alert.forEach(item => item.classList.remove("select-hide"));
+}); });
+
+ButtonDiscount.forEach(item => { item.addEventListener("click", () => {
+    Alert.forEach(item => item.classList.remove("select-hide"));
+}); });
+
+
+/*меняем цвет креста на черный*/
+const crossAlertSVG = document.querySelectorAll('.svgInternalID');
+let i = 0;
+while (i < crossAlertSVG.length)
+{
+    crossAlertSVG[i].setAttribute("stroke", "black");
+    i++;
+}
+
+/*при нажатии на крест скрываем*/
+const crossAlert = document.querySelectorAll('.alert__cross');
+crossAlert.forEach(item => { item.addEventListener("click", () => {
+    Alert.forEach(item => item.classList.add("select-hide"));
+});
+});
+
+const crossDiscount = document.querySelectorAll('.offer__close-discount');
+const discount = document.querySelectorAll('.offer__discount');
+
+crossDiscount.forEach(item => { item.addEventListener("click", () => {
+    discount.forEach(item => item.style.display = 'none');
+});
+});
+
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+const noticeMobile = document.querySelector('.diagnoctics__notice-mobile');
+const noticeDesktop = document.querySelector('.diagnoctics__notice-desktop');
+
+const teamPerson = document.querySelectorAll('.team__person');
+
+const feedbackItem = document.querySelectorAll('.feedback__item');
+
+if (screenWidth <= 1024) {
+
+    noticeMobile.style.display = 'flex';
+    noticeDesktop.style.display = 'none';
+
+    if (teamPerson.length === 3) {
+        teamPerson[2].style.display = 'none';
+    }
+
+    if (feedbackItem.length === 2) {
+        feedbackItem[1].style.display = 'none';
+    }
+}
+else if (screenWidth > 1024) {
+
+    noticeMobile.style.display = 'none';
+    noticeDesktop.style.display = 'flex';
+
+    if (teamPerson.length < 3) {
+        teamPerson[2].style.display = 'block';
+    }
+
+    if (feedbackItem.length < 2) {
+        feedbackItem[1].style.display = 'block';
+    }
+}
