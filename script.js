390f65c634a3e950f8d61074f3340e022a90edc4
Milana Khairullina <hmm2002@yandex.ru>
2023-07-17 01:58:06 +0300

    add: add indicator changes by back btn and slider with team

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-/*----Custom select---*/
+/*----CUSTOM SELECT---*/
 const selectHeader = document.querySelectorAll('.select__header');
 const selectItem = document.querySelectorAll('.select__item');
 const selectBody = document.querySelectorAll('.select__body');
@@ -76,16 +76,45 @@ crossDiscount.forEach(item => { item.addEventListener("click", () => {
 });
 });
 
-//const screenWidth = window.screen.width;
-//const screenHeight = window.screen.height;
-
 /*---------Нажатие на ДАЛЕЕ и изменение активного индикатора ---------*/
 const defaultNavNextBtn = document.querySelectorAll('.nav__right');
+
+const defaultNavLeftBtn = document.querySelectorAll('.nav__left');
 const defaultNavIndicatorFeedback = document.querySelector('.nav__indicator.feedback');
 const defaultNavIndicatorTeam = document.querySelector('.nav__indicator.team');
+const teamPersons = document.querySelectorAll('.team__person');
+const teamWorkers = document.querySelector('.team__workers');
 
+
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+
+let teamPersonWidth = window.getComputedStyle(teamPersons[0]).minWidth.split('px')[0];
+teamPersonWidth = Number(teamPersonWidth);
+
+if (screenWidth > 505 || screenWidth < 361)
+    teamPersonWidth+=30;
+else
+    teamPersonWidth+=60;
+
+console.log(screenWidth);
+console.log(teamPersonWidth);
+
+let position = 0;
+const checkBtns = () => {
+    
+    defaultNavLeftBtn[0].disabled = position === 0;
+    console.log (position);
+    console.log(-((teamPersons.length - 3) * teamPersonWidth));
+    const defaultNavNextBtnTrue = defaultNavNextBtn[0].getElementsByTagName('button');
+    defaultNavNextBtnTrue[0].disabled = position <= -((teamPersons.length - 3) * teamPersonWidth);
+};
+checkBtns();
 defaultNavNextBtn.forEach(item => { item.addEventListener("click", () => {
-   if (item.classList.contains('feedback'))
+   
+    //checkBtns();
+    if (item.classList.contains('feedback'))
    {
     for (let i = 0; i < defaultNavIndicatorFeedback.children.length; i++) //проходимся по всем кружочкам и ищем эктив
     {
@@ -127,32 +156,66 @@ defaultNavNextBtn.forEach(item => { item.addEventListener("click", () => {
     {
         if (defaultNavIndicatorTeam.children[i].className === 'active') 
             {
-                    const active = defaultNavIndicatorTeam.children[i];
-                    const deepCopy = active.cloneNode(true);
+                const active = defaultNavIndicatorTeam.children[i];
+                const deepCopy = active.cloneNode(true);
 
-                    if (i!==3) /*если это не последний кружок*/
-                    {
-                        active.after(deepCopy); //добавляем active после текущего active
-                        active.remove(); //удаляем старый active
+                active.after(deepCopy); //добавляем active после текущего active
+                active.remove(); //удаляем старый active
 
-                        const activenew = defaultNavIndicatorTeam.querySelector('.active'); //находим новый active
+                const activenew = defaultNavIndicatorTeam.querySelector('.active'); //находим новый active
 
-                        const navPage = document.createElement('div');
-                        navPage.classList.add('nav__page');
+                const navPage = document.createElement('div');
+                navPage.classList.add('nav__page');
 
-                        activenew.before(navPage); //добавляем перед ним (взамен прошлого active) обычный кружок
+                activenew.before(navPage); //добавляем перед ним (взамен прошлого active) обычный кружок
 
-                        const navPagesAll = defaultNavIndicatorTeam.querySelectorAll('.nav__page');
-                        navPagesAll[navPagesAll.length - 1].remove(); //удаляем кружок в конце
-                    }
-                    else //если мы на концеы
-                    {
-                        defaultNavIndicatorTeam.prepend(deepCopy); //добавляем active в самое начало
-                        active.remove(); //удаляем старый из конца
-                    }
-                    break;
+                const navPagesAll = defaultNavIndicatorTeam.querySelectorAll('.nav__page');
+                navPagesAll[navPagesAll.length - 1].remove(); //удаляем кружок в конце
+                
+                break;
             }   
     }
 
+    //if (screenWidth > 1100) {
+        position-=teamPersonWidth;
+        teamWorkers.style.transform = `translateX(${position}px)`;
+
+    //}
+
    }
+   checkBtns();
 }); });
+
+defaultNavLeftBtn.forEach(item => { item.addEventListener("click", () => {
+  //  checkBtns();
+    if  (item.classList.contains('feedback'))
+    {
+
+    }
+    
+    else if (item.classList.contains('team'))
+    {
+
+        for (let i = 0; i < defaultNavIndicatorTeam.children.length; i++)
+        {
+            if (defaultNavIndicatorTeam.children[i].className === 'active') 
+                {
+                    const active = defaultNavIndicatorTeam.children[i];
+
+                    defaultNavIndicatorTeam.children[i-1].remove(); //удаляем старый active
+    
+                    const navPage = document.createElement('div');
+                    navPage.classList.add('nav__page');
+    
+                    active.after(navPage); //добавляем до него (взамен прошлого active) обычный кружок
+                    break;
+                }   
+        }
+
+        position+=teamPersonWidth;
+        teamWorkers.style.transform = `translateX(${position}px)`;
+
+        checkBtns();
+    }
+}); });
+
